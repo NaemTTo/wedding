1dcc8cb8bfd3a4e515636f496814c88fe78276fe
SnoopyKim <snoopykim0@gmail.com>
2022-10-31 18:09:03 +0900

    Prettier 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import useDialog from '../dialogs/store'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import useDialog from '../dialogs/store';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const [showAlertDialog, showConfirmDialog] = useDialog((state) => [state.showAlertDialog, state.showConfirmDialog]);
+  const [showAlertDialog, showConfirmDialog] = useDialog((state) => [
+    state.showAlertDialog,
+    state.showConfirmDialog,
+  ]);
   return (
     <div>
       <Head>
@@ -14,8 +17,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.weddingImg}>
-        </div>
+        <div className={styles.weddingImg}></div>
         <div className={styles.weddingText}>
           <div className={styles.weddingName}>
             <p className={styles.weddingFirstName}>홍길동</p>
@@ -30,8 +32,27 @@ const Home: NextPage = () => {
           <div className={styles.weddingInviteWrap}>
             <h4>가장 소중하고 아름다운 순간에 초대합니다!</h4>
             <div className="">
-              <button onClick={() => showAlertDialog('Test Alert Dialog', 'This is simple alert dialog for test.')}>Alert</button>
-              <button onClick={() => showConfirmDialog('Test Confirm Dialog', 'This is simple confirm dialog for test.', () => {})}>Confirm</button>
+              <button
+                onClick={() =>
+                  showAlertDialog(
+                    'Test Alert Dialog',
+                    'This is simple alert dialog for test.',
+                  )
+                }
+              >
+                Alert
+              </button>
+              <button
+                onClick={() =>
+                  showConfirmDialog(
+                    'Test Confirm Dialog',
+                    'This is simple confirm dialog for test.',
+                    () => {},
+                  )
+                }
+              >
+                Confirm
+              </button>
             </div>
           </div>
         </section>
@@ -45,8 +66,8 @@ const Home: NextPage = () => {
           powered by sujin
         </a>
       </footer>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
